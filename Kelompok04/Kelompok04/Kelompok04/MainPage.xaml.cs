a81272d6683609e75d970c87fd44209c38e0b93b
Ratna Yuli Himawati <ryh98@users.noreply.github.com>
2018-10-29 15:21:21 +0700

    modul 1

---
+++ Kelompok04/Kelompok04/Kelompok04/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Kelompok04;
+using System;
+using Xamarin.Forms;
+
+namespace Kelompok04
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new LoginPage(), this);
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ Kelompok04/Kelompok04/Kelompok04/Model/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kelompok04.Model
+{
+    public static class UserLogin
+    {
+        public static string Username = "Kelompok04";
+        public static string Password = "12345";
+    }
+}
